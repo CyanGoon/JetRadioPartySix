7511809f79058938cbb0e17bf5118d63d2352e53
robert <robertnmoffat@gmail.com>
2018-01-27 13:40:24 -0800

    started tile spawning

--- Jet Radio Party Six- Chef Transmission- To and From the bottom of the Pit/Assets/Scripts/ControllerScript.cs
+++ Jet Radio Party Six- Chef Transmission- To and From the bottom of the Pit/Assets/Scripts/ControllerScript.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControllerScript : MonoBehaviour {
+    public GameObject gameTile;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private GameObject tile;
+    private TileScript tileScript;
+
+    // Use this for initialization
+    void Start () {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                tile = Instantiate(gameTile, new Vector3(x, 0, y), Quaternion.identity);
+                tileScript = (TileScript)tile.GetComponent<TileScript>();
+                tileScript.UpdateTexture(1);
+                //tileScript.testFunction();
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Jet Radio Party Six- Chef Transmission- To and From the bottom of the Pit/Assets/Scripts/TileScript.cs
+++ Jet Radio Party Six- Chef Transmission- To and From the bottom of the Pit/Assets/Scripts/TileScript.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class TileScript : MonoBehaviour {
     public GameObject tileQuad;
 
+    int currentTexture;
+
+    public Texture2D[] textures;
+
     Material tileMaterial;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    
+    //Update the current texture
+    public void UpdateTexture(int newTexture) {
+        tileMaterial = tileQuad.GetComponent<Renderer>().material;
+        currentTexture = newTexture;
+        tileMaterial.mainTexture = textures[newTexture];
+    }
+
+    public void testFunction() {
+        Debug.Log("test function works");
+    }
 }
